422e28a4df0abe7a25d34d9d5034cac38272bc4b
ash-rukshan <ashen@mydigitaloffice.ca>
2023-07-20 11:13:43 +0530

    Invoice module

--- src/test/test.java
+++ src/test/test.java
@@ -1,2 +1,23 @@
-//Validate NIC in Customer module
+// Validate NIC in Customer module
 
+// Get Column Total in a table JS
+// <script>
+// $(document).ready(function () {
+//
+// var sum = 0;
+//
+// $('.TotalItem').each(function () {
+// var value = parseInt($(this).text(), 10);
+// if (!isNaN(value)) {
+// sum += value;
+// }
+// });
+//
+// console.log(sum); $("#todayTotal").html(sum.toFixed(2))
+//
+// });
+//
+// </script>
+// <th class="TotalItem"
+// data-th-text="${itemNameAndCount.itemPrice}*${itemNameAndCount.itemSellCount}"></th
+// >
